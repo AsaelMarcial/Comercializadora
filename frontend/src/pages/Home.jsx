b02085862a5fed9afe68f4367160c24d940f253b
Asael Marcial <asael_marcial@hotmail.es>
2024-09-26 02:55:18 -0600

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,11 @@
 import { useState, useEffect, useRef } from 'react'
-import { Player } from '@lottiefiles/react-lottie-player'
 import '../css/home.css'
 import NavigationTitle from '../components/NavigationTitle'
 
 const Home = () => {
 	const [currentDate, setcurrentDate] = useState(new Date())
-	const phrases = [
-		'"Cocinar con amor te alimenta el alma 🧡"',
-		'"En esta cocina pasan cosas mágicas ✨"',
-		'"La cocina es el corazón de la casa ☕️"',
-		'"Hoy me comeré la vida a mordisquitos 🌮"',
-		'"Barriga llena, corazón contento 💖"',
-		'"Si algo te hace feliz cómete el doble 😋"',
-		'"Llena la vida con los mejores ingredientes 🍲"',
-	]
-	const randomIndex = useRef(Math.floor(Math.random() * (phrases.length - 1)))
-	const randomPhrase = phrases[randomIndex.current]
-
 	useEffect(() => {
-		document.title = 'ComedorUV'
+		document.title = 'Comercializadora ORZA'
 		const timerId = setInterval(refreshClock, 1000)
 
 		return () => {
@@ -34,15 +21,26 @@ const Home = () => {
 	return (
 		<>
 			<NavigationTitle menu="Inicio"/>
-			<div className='contenedor-inicio'>
-				<div className='animacion-inicio'>
-					<Player
-						autoplay
-						loop
-						background='transparent'
-						src='https://assets3.lottiefiles.com/packages/lf20_jbt4j3ea.json'
-						renderer='svg'
-					></Player>
+			<div className='contenedor-inicio' style={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				height: '100%',
+				width: '100%',
+				textAlign: 'center'
+			}}>
+				<div>
+					<img 
+						src="\Logo DISTRIBUIDORA Orza FONDO AZUL.png" 
+						alt="Logo DISTRIBUIDORA Orza"
+						style={{ 
+							width: '100%', 
+							maxWidth: '300px', 
+							height: 'auto',
+							marginBottom: '20px' // Añade espacio entre la imagen y el texto
+						}} 
+					/>
 				</div>
 				<div className='informacion-inicio'>
 					<span className='texto-fecha'>
@@ -52,10 +50,10 @@ const Home = () => {
 						{currentDate.toLocaleTimeString()} <br />
 					</span>
 					<span className='texto-inicio'>
-						Personas beneficiadas: <br />
+						Bienvenido: <br />
 					</span>
 					<span className='texto-frases'>
-						{randomPhrase}
+						Asael
 					</span>
 				</div>
 			</div>
